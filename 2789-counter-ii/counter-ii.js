de5d41d40373183716cd41e92184fa48a81c2f49
Abdelmageed Hamdi <68792061+abdelmageed2062019@users.noreply.github.com>
2024-05-07 21:40:43 +0300

    Time: 56 ms (71.92%) | Memory: 51.5 MB (60.63%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let counter = init;
+    return {
+        increment: () => {
+           return ++counter;
+        },
+        decrement: () => {
+           return --counter;
+        },
+        reset: () => {
+            return counter = init;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
